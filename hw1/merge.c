bfa938f07ebaa9bacaefb77a879dfd691ec232b5
Jacky <gomjellie@gmail.com>
2020-03-30 23:28:08 +0900

    fin merge

---
+++ hw1/merge.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        puts("It needs 2 arguments");
+        puts("Usage: ./a.out filename1 filename2"); return 1;
+    }
+
+    int fd1, fd2;
+    char c;
+    fd1 = open(argv[1], O_RDWR| O_CREAT| O_APPEND, 0644);
+    if (fd1 == -1) {
+        printf("Can't open file %s\n", argv[1]); return 1;
+    }
+
+    fd2 = open(argv[2], O_RDONLY);
+    if (fd2 == -1) {
+        printf("Can't open file %s\n", argv[2]);
+    }
+
+    while (read(fd2, &c, 1) != 0) {
+        write(fd1, &c, 1);
+    }
+}
