0fce7f6618335d8bf13b128b3ee5317c4a9ea6f8
Jacky <gomjellie@gmail.com>
2020-03-31 02:25:03 +0900

    remove useless lseek

--- hw1/copy.c
+++ hw1/copy.c
@@ -6,7 +6,7 @@
 extern int errno;
 
 #define BUF_SIZE 100
-#define OVERFLOW_PADDING 8
+#define BUF_PADD 8
 
 enum args {
     SRC_FILENAME = 1,
@@ -19,16 +19,13 @@ int main(int argc, char* argv[]) {
         puts("Usage: ./a.out source_filename target_filename"); return 1;
     }
 
-    char buf[BUF_SIZE + OVERFLOW_PADDING];
+    char buf[BUF_SIZE + BUF_PADD];
     int src_fd, tar_fd;
     src_fd = open(argv[SRC_FILENAME], O_RDONLY);
     tar_fd = open(argv[TAR_FILENAME], O_RDWR | O_CREAT | O_TRUNC, 0644);
 
     if (src_fd == -1) perror("Error: ");
     if (tar_fd == -1) perror("Error: ");
-
-    lseek(src_fd, 0, SEEK_SET);
-    lseek(tar_fd, 0, SEEK_SET);
 
     size_t size = 0;
     while ((size = read(src_fd, buf, BUF_SIZE)) > 0)
