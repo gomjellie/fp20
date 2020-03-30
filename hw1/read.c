0fed96812a525f76187a293ae83cfc72b21a96d6
Jacky <gomjellie@gmail.com>
2020-03-30 22:56:44 +0900

    fin read.c

---
+++ hw1/read.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+extern int errno;
+
+#define FILENAME   1
+#define OFFSET     2
+#define READ_BYTE  3
+
+
+int main(int argc, char *argv[]) {
+    int fd;
+    char c;
+    long int cnt = 0;
+    if (argc != 4) {
+        puts("It needs 3 arguments");
+        puts("Usage: ./a.out filename offset bytes_to_read"); return 1;
+    }
+
+    fd = open(argv[FILENAME], O_RDONLY);
+
+    if (fd == -1) {
+        puts("Error opening file"); return 1;
+    }
+
+    lseek(fd, (off_t)atol(argv[OFFSET]), SEEK_SET);
+
+    while (read(fd, &c, 1) != 0 && cnt < atol(argv[READ_BYTE])) {
+        cnt ++;
+        write(1, &c, 1);
+    }
+
+    if (close(fd) == -1) {
+        perror("Error: ");
+    }
+}
