112ebd0b91eda0e95170d583825786e50a74da8f
Jacky <gomjellie@gmail.com>
2020-04-07 05:52:12 +0900

    fin read_seq

--- hw2/read_seq.c
+++ hw2/read_seq.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
-#include <unistd.h> // for sleep()
+#include <unistd.h>
+#include <fcntl.h>
 //필요하면 header file 추가 가능
 
-//
-// argv[1]: 레코드 파일명
-//
+#define BUF_SIZE 100
+#define BUF_PADD 32
+
+enum args {
+    SRC_FILENAME = 1,
+};
+
+void sread(char* source_filename) {
+    // sequential read
+    char buf[BUF_SIZE + BUF_PADD];
+    int src_fd;
+    src_fd = open(source_filename, O_RDONLY);
+    if (src_fd < 0) {perror("Error"); exit(1);}
+
+    size_t size = 0;
+    while ((size = read(src_fd, buf, BUF_SIZE)) > 0){
+        write(1, buf, size);
+    }
+    if (close(src_fd) == -1){perror("Error"); exit(1);}
+    puts("");
+}
+
 int main(int argc, char **argv) {
     // 표준입력으로 받은 레코드 파일에 저장되어 있는 전체 레코드를 "순차적"으로 읽어들이고, 이때
     // 걸리는 시간을 측정하는 코드 구현함
@@ -17,7 +38,8 @@ int main(int argc, char **argv) {
     double diffTime;
     gettimeofday(&startTime, NULL);
 
-    
+    sread(argv[SRC_FILENAME]);
+
     gettimeofday(&endTime, NULL);
     diffTime = (endTime.tv_sec - startTime.tv_sec) * 1000 * 1000 + (endTime.tv_usec - startTime.tv_usec);
     printf("timecost: %.0f us", diffTime);
